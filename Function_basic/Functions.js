ced9ee40355d2595e8414b15fab4d48dc6210fd7
Irenevs83 <80913299+Irenevs83@users.noreply.github.com>
2021-05-08 14:43:57 +0200

    Add files via upload

---
+++ Function_basic/Functions.js
@@ -0,0 +1,26 @@
+const makePizza = function () {
+    console.log('Pak een pizzabodem');
+    console.log('Verdeel de tomatensaus hierover');
+    console.log('strooi er geraspte kaas over');
+    console.log('Beleg met plakjes salami');
+    console.log('Beleg met plakjes champignon');
+    console.log('Beleg met rode en groene paprika');
+    console.log('Strooi er een beetje gedroogde oregano over');
+    console.log('Leg de pizza in de oven');
+    console.log('Verwarm de pizza op 200 graden in 10 - 12 minuten.');
+}
+const makeSushi = function () {
+    console.log('Kook de rijst volgens de aanwijzigingen op de verpakking');
+    console.log('Snijd de komkommer in lange dunne repen');
+    console.log('Snijd de zalm in lange dunne repen');
+    console.log('Pak een bamboematje en leg daar een stuk vershoudfolie op');
+    console.log('Verdeel hierover de rijst en druk deze goed aan.');
+    console.log('Verdeel de komkommer en de zalm over 1 lengte van de rijst');
+    console.log('Rol alles op en snijd de sushi aan plakken');
+}
+// de functie werkt nog niet omdat ik hem nog moet callen.
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
